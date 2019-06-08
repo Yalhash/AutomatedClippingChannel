c602f48b15aa17deac7bbda69dbf1ec145737652
Yalhash <yalhash@DESKTOP-AEGL8LS.localdomain>
2019-06-08 14:21:48 -0400

    updated files

--- sortParseDown.py
+++ sortParseDown.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from parseChannels import getVideosMetaData
-from parseChannels import loadChannels
 from videoDownload import videoDownload
 
 
@@ -9,7 +8,30 @@ from videoDownload import videoDownload
 #Goal is to get 10:00 worth of content sorted by number of views
 #also try to avoid repeats -> avoid clips from the same stream
 
+def loadGame():
+    '''
+    returns a string of the game we want to download from the game.txt file
+    '''
+    with open("game.txt") as gameFile:  
+        return gameFile.read()
+
+def loadChannels():
+    '''
+    returns a list of all the channels in the channels.csv file
+    '''
+    with open("channels.csv") as channelFile:
+        return channelFile.read().split(',')
+
+
+
 def differentStream(metaList, metaTuple):
+    '''
+    metaList: a list of metadata tuples
+    metaTuple: an instance of metadata
+    the function returns False if the channel name and date of the clip are equal,
+    and True if they are not, 
+    used to avoid downloading the same clip under a different name
+    '''
     for i in metaList:
         if i[4] == metaTuple[4] and i[5] == metaTuple[5]:
             return False
@@ -23,16 +45,19 @@ def getAllVids():
     '''
     downloads 10 minutes worth of content, or until content runs out
     into the assets folder
+    returns: a list of strings designating the filenames of each downloaded file, 
+    sorted from most to least views
     '''
     #get all of the metadata
     driver = webdriver.Chrome()   
     ChannelList = loadChannels()
+    gameName = loadGame()
     allMetaData = []
 
 
 
     for channel in ChannelList:
-        allMetaData.extend(getVideosMetaData(driver, channel))
+        allMetaData.extend(getVideosMetaData(driver, channel, gameName))
 
     #sort by views
     allMetaData.sort(key=lambda t: t[3], reverse=True)
@@ -49,14 +74,15 @@ def getAllVids():
             downloadList.append(allMetaData[index])
             totalTime += allMetaData[index][2]
         index += 1
-    
+    fileList = []
     for i in range(len(downloadList)):
         
         fName = downloadList[i][1] + '_' + downloadList[i][5] + '_'+str(downloadList[i][2]) + '_'+str(downloadList[i][3]) + '_'+downloadList[i][4] 
-        
+        fileList.append(fName)
         videoDownload(downloadList[i][0], fName, driver)
     
-    driver.close()    
+    driver.close()  
+    return fileList
 
 
 if __name__ == "__main__":
